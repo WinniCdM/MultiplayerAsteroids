bc2d0b1e27aae44bf5471cfe7267c46e39349990
Ben Winslett <winslett.benjamin@gmail.com>
2019-04-13 15:54:12 -0600

    Fun stuff, for the window

--- scripts/client/loader.js
+++ scripts/client/loader.js
@@ -44,10 +44,10 @@ MyGame.loader = (function() {
         }],
         assetOrder = [{
             key: 'player-self',
-            source: 'assets/playerShip1_blue.png'
+            source: 'assets/images/players/playerShip1_blue.png'
         }, {
             key: 'player-other',
-            source: 'assets/playerShip1_red.png'
+            source: 'assets/images/players/playerShip1_red.png'
         }];
 
     //------------------------------------------------------------------
