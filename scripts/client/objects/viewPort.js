ccb94aefc17de9d5a6b6414236318286313d6ca8
Ben Winslett <winslett.benjamin@gmail.com>
2019-04-14 14:40:16 -0600

    ViewPort nudging complete

--- scripts/client/objects/viewPort.js
+++ scripts/client/objects/viewPort.js
@@ -6,6 +6,9 @@
 MyGame.components.ViewPort = function(playerGlobalPosition) {
     'use strict';
     let that = {};
+
+    let initialSetupComplete = false;
+
     let position = { // world units 0-10
         x: 0,
         y: 0
@@ -35,7 +38,19 @@ MyGame.components.ViewPort = function(playerGlobalPosition) {
     });
 
     that.update = function(elapsedTime) {
-        // TODO: update position of viewport based on player position
+        updateViewPortAndPlayerPosition();
+    };
+
+    //------------------------------------------------------------------
+    //
+    // Nudges the viewport within the entire world map according to the
+    // player's position. 
+    //
+    // If the viewport is at the edge of the world map, the viewport
+    // won't move and allow the player to move to the edge.
+    //
+    //------------------------------------------------------------------
+    let updateViewPortAndPlayerPosition = function(){
         playerLocalPosition.x = playerGlobalPosition.x - position.x 
         playerLocalPosition.y = playerGlobalPosition.y - position.y
 
@@ -45,21 +60,42 @@ MyGame.components.ViewPort = function(playerGlobalPosition) {
             initialSetupComplete = true;
         }
 
-        if (playerLocalPosition.x < slidingWindowMargin){ 
-            //move viewport to the left
-            console.log("HIT WINDOW X MARGIN ", playerLocalPosition);
-        } else if (playerLocalPosition.x > 1 - slidingWindowMargin){
-            //move to the right
-            console.log("HIT WINDOW X MARGIN ", playerLocalPosition);
+        if (playerLocalPosition.x < slidingWindowMargin){ // nudge left
+            position.x -= slidingWindowMargin - playerLocalPosition.x; //nudges the viewport to the left
+            if (position.x < 0){ // if its beyond the left bound, return viewport to left bound, and do not update player local position
+                position.x = 0;
+            }
+            else{   //else update player local position
+                playerLocalPosition.x = slidingWindowMargin;
+            }
+        } else if (playerLocalPosition.x > 2 - slidingWindowMargin){ // nudge right
+            position.x += playerLocalPosition.x - (2 - slidingWindowMargin); //nudges the viewport to the right
+            if (position.x + 2 > 10){ // if its beyond the right bound, return viewport to left bound, and do not update player local position
+                position.x = 10;
+            }
+            else{   //else update player local position
+                playerLocalPosition.x = 2 - slidingWindowMargin;
+            }
         }
-        if (playerLocalPosition.y < slidingWindowMargin){
-            //move up
-            console.log("HIT WINDOW Y MARGIN ", playerLocalPosition);
-        } else if (playerLocalPosition.y > 1 - slidingWindowMargin){
-            //move down
-            console.log("HIT WINDOW Y MARGIN ", playerLocalPosition);
+
+        if (playerLocalPosition.y < slidingWindowMargin){ // nudge up
+            position.y -= slidingWindowMargin - playerLocalPosition.y; //nudges the viewport up
+            if (position.y < 0){ // if its beyond the north bound, return viewport to north bound, and do not update player local position
+                position.y = 0;
+            }
+            else{   //else update player local position
+                playerLocalPosition.y = slidingWindowMargin;
+            }
+        } else if (playerLocalPosition.y > 1 - slidingWindowMargin){ //nudge down
+            position.y += playerLocalPosition.y - (1 - slidingWindowMargin); //nudges the viewport down
+            if (position.y + 1 > 10){ // if its beyond the south bound, return viewport to sotuh bound, and do not update player local position
+                position.y = 10;
+            }
+            else{   //else update player local position
+                playerLocalPosition.y = 2 - slidingWindowMargin;
+            }
         }
-    };
+    }
 
     return that;
 };
